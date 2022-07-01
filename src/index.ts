29938611aa115da7efc374df4ec804b0567d5f5d
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-07-01 17:07:51 +0200

    public decorator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 'use strict'
 
 export * from './real.ip'
+export * from './public.decorator'
 export * from './raw-body.decorator'
---
+++ src/public.decorator.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+import { applyDecorators, SetMetadata } from '@nestjs/common'
+
+export const META_UNPROTECTED = 'unprotected'
+
+export const Public = () => applyDecorators(
+  SetMetadata(META_UNPROTECTED, true),
+)

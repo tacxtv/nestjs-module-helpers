5d03c6173a1439d14dd43e38ed0b606ede073d62
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-08-19 11:00:15 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 export * from './abstract.controller'
 export * from './abstract.service'
+export * from './abstract.command.service'
 export * from './real-ip.decorator'
 export * from './public.decorator'
 export * from './parseObjectId.pipe'
